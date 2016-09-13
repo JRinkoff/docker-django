423493ca628cd033484d4434322c2ca43f8a60b2
Mark Doggen <markdoggen@gmail.com>
2016-09-13 14:12:51 +0200

    Update deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
+rm -rf .git # Remove .git folder because we want to use this as a project
 mv project $1
 find . -name '*.py' -o -name '*.conf' -o -name '*.vcl' -o -name 'entrypoint.sh' -type f | xargs perl -p -i -e "s/{{project}}/$1/g"
