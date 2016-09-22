4bc384be74cd7853244be5410ece38fd7b3a2d40
Mark Doggen <markdoggen@gmail.com>
2016-09-22 11:25:24 +0200

    Update clear_cache management command to also enable clearing Redis and Varnish from Django.

--- app/management/commands/clear_cache.py
+++ app/management/commands/clear_cache.py
@@ -1,13 +1,13 @@
 from django.core.management.base import BaseCommand
+from subprocess import call
 from django.core.cache import caches
-import os
-import glob
+import os, glob
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('clear', nargs='+', type=str, choices=['all', 'default', 'templates', 'cachalot'])
+        parser.add_argument('clear', type=str, choices=['all', 'default', 'templates', 'cachalot', 'varnish', 'redis'])
 
     def clear_templates(self):
         files = glob.glob(os.path.join(__location__, '../../../cache/template_fragments/*.djcache'))
@@ -20,15 +20,28 @@ class Command(BaseCommand):
         keys = cache.delete_many(cache.keys('*'))
         print 'Cache "{name}" cleared. {keys} keys deleted.'.format(name=name, keys=keys)
 
+    def clear_varnish(self):
+        call(['varnishadm', '-T', 'varnish:6082', '-S', '/app/docker/varnish/secret', 'ban.url', '.'])
+        print 'Varnish cleared'
+
+    def clear_redis(self):
+        call(['redis-cli', '-s', '/app/docker/etc/redis.sock', 'flushall'])
+        print 'Redis cleared'
+
     def handle(self, *args, **options):
-        for clear in options['clear']:
-            if clear == 'all':
-                self.clear_cache('default')
-                if 'templates' not in options['clear']:
-                    self.clear_templates()
-            elif clear == 'default':
-                self.clear_cache('default')
-            elif clear == 'cachalot':
-                self.clear_cache('cachalot')
-            elif clear == 'templates':
-                self.clear_templates()
+        if options['clear'] == 'all':
+            self.clear_cache('default')
+            self.clear_cache('cachalot')
+            self.clear_templates()
+            self.clear_redis()
+            self.clear_varnish()
+        elif options['clear'] == 'default':
+            self.clear_cache('default')
+        elif options['clear'] == 'cachalot':
+            self.clear_cache('cachalot')
+        elif options['clear'] == 'templates':
+            self.clear_templates()
+        elif options['clear'] == 'varnish':
+            self.clear_varnish()
+        elif options['clear'] == 'redis':
+            self.clear_redis()
