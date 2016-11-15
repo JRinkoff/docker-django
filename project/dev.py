35c02396437d1bb8438229fb03e99958cfb2b343
Mark Doggen <markdoggen@gmail.com>
2016-11-15 10:47:32 +0100

    Don't log static files in development.

--- project/dev.py
+++ project/dev.py
@@ -5,3 +5,18 @@ INTERNAL_IPS.append(DOCKER_IPS)
 ALLOWED_HOSTS += ['.{{project}}.dev']
 
 DEBUG = True
+
+# Don't log static files
+# Grab the original log_message method.
+_log_message = WSGIRequestHandler.log_message
+
+
+def log_message(self, *args):
+    # Don't log if path starts with /static/
+    if self.path.startswith('/static/'):
+        return
+    else:
+        return _log_message(self, *args)
+
+# Replace log_message with our custom one.
+WSGIRequestHandler.log_message = log_message
